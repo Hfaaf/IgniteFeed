054f57aff90f2b0534e0368cfb9277ce8a06d87f
Hfaaf <heitorfariaspinheirodoamaral@gmail.com>
2025-03-20 13:26:37 -0300

    abacate

--- src/App.jsx
+++ src/App.jsx
@@ -15,44 +15,47 @@ const posts = [
       role: 'Web Developer',
     },
     content: [
-        { type: 'paragraph', content: 'Fala galeraa 👋'},
-        { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀 '},
-        { type: 'link', content:'jane.design/doctorcare'},
-      ],
-      publishedAt: new Date('2025-03-11 14:00:00')
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀 ' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2025-03-11 14:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://wallpapers.com/images/hd/funny-cartoon-of-mike-wazowski-byeruzljofv0ydip.jpg',
+      name: 'mike wazowski',
+      role: 'Assustador Profissional',
     },
-    {
-      id: 2,
-      author: {
-        avatarUrl: 'https://wallpapers.com/images/hd/funny-cartoon-of-mike-wazowski-byeruzljofv0ydip.jpg',
-        name: 'mike wazowski',
-        role: 'Assustador Profissional',
-      },
-      content: [
-          { type: 'paragraph', content: 'Fala monstrada'},
-          { type: 'paragraph', content: 'Acabei de assustar muito mais gente e coloquei no meu portifa profissional. Assustei elas com o meu amigão '},
-          { type: 'mark', content: '@JSullivan'},
-          { type: 'link', content:'jane.design/doctorcare'},
-        ],
-        publishedAt: new Date('2025-03-1 14:00:00')
-      },
+    content: [
+      { type: 'paragraph', content: 'Fala monstrada' },
+      { type: 'paragraph', content: 'Acabei de assustar muito mais gente e coloquei no meu portifa profissional. Assustei elas com o meu amigão ' },
+      { type: 'link', content: '@JSullivan' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2025-03-1 14:00:00')
+  },
 ]
 
 export function App() {
   return (
     <div>
       <Header />
+
+
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
           {posts.map(post => {
             return (
-            <Post 
-              author={post.author}
-              content={post.content}
-              publishedAt={post.publishedAt}
-            />
-          )
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
           })}
         </main>
       </div>
--- src/components/post.jsx
+++ src/components/post.jsx
@@ -8,82 +8,80 @@ import { Comment } from './Comment'
 
 import styles from './Post.module.css'
 
-export function Post({author, publishedAt, content }) {
-    const [comments, setComments] = useState([
-        'Post muito bacana, hein?'
-    ])
+export function Post({ author, publishedAt, content }) {
+  const [comments, setComments] = useState([
+    'Post muito bacana, hein?'
+  ])
 
-    const [newCommentText, setNewCommentText] = useState('')
+  const [newCommentText, setNewCommentText] = useState('')
 
-    const publishedDateFormat = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
-        locale: ptBR,
-    })
+  const publishedDateFormat = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR,
+  })
 
-    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
-        locale: ptBR,
-        addSuffix: true,
-    })
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  })
 
-    function handleCreateNewComment() {
-        event.preventDefault();
+  function handleCreateNewComment() {
+    event.preventDefault();
 
-        setComments([...comments, newCommentText]) 
-        setNewCommentText('')
-    }
+    setComments([...comments, newCommentText])
+    setNewCommentText('')
+  }
 
-    function handleNewCommentChange() {
-        setNewCommentText(event.target.value)
-    }
+  function handleNewCommentChange() {
+    setNewCommentText(event.target.value)
+  }
 
-    return(
-        <article className={styles.post}>
-            <header>
-                <div className={styles.author}>
-                    <Avatar src = {author.avatarUrl}/>
-                    <div className={styles.authorInfo}>
-                        <strong>{author.name}</strong>
-                        <span>{author.role}</span>
-                    </div>
-                </div>
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <Avatar src={author.avatarUrl} />
+          <div className={styles.authorInfo}>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
+          </div>
+        </div>
 
-                <time title={publishedDateFormat} dateTime={publishedAt.toISOString()}>
-                {publishedDateRelativeToNow}
-                </time>
-            </header>
+        <time title={publishedDateFormat} dateTime={publishedAt.toISOString()}>
+          {publishedDateRelativeToNow}
+        </time>
+      </header>
 
-            <div className={styles.content}>
-                {content.map(line => {
-                    if (line.type === 'paragraph') {
-                        return <p>{line.content}</p>
-                    }
-                    else if (line.type === 'link') {
-                        return <p><a href='#'>{line.content}</a></p>
-                    }
-                    else if (line.type === 'mark') {
-                        return <pre><a href='#'>{line.content}</a></pre>
-                    }
-                })}
-            </div>
+      <div className={styles.content}>
+        {content.map(line => {
+          if (line.type === 'paragraph') {
+            return <p key={line.content}>{line.content}</p>
+          }
+          else if (line.type === 'link') {
+            return <p key={line.content}><a href='#'>{line.content}</a></p>
+          }
 
-            <form onSubmit={handleCreateNewComment} className={styles.comments}>
-                <strong>Deixe o seu feedback</strong>
+        })}
+      </div>
 
-                <textarea 
-                    name='comment'
-                    placeholder='Deixe seu comentario'
-                    value={newCommentText}
-                    onChange={handleNewCommentChange}
-                />
-                <footer>
-                    <button type='submit'>Publicar</button>
-                </footer>
-            </form>
+      <form onSubmit={handleCreateNewComment} className={styles.comments}>
+        <strong>Deixe o seu feedback</strong>
 
-            <div className={styles.commentList}>
-                {comments.map(comment => {
-                    return <Comment content={comment} />
-                })}
-            </div>
-        </article>
-    )
+        <textarea
+          name='comment'
+          placeholder='Deixe seu comentario'
+          value={newCommentText}
+          onChange={handleNewCommentChange}
+        />
+        <footer>
+          <button type='submit'>Publicar</button>
+        </footer>
+      </form>
+
+      <div className={styles.commentList}>
+        {comments.map(comment => {
+          return <Comment key={content} content={comment} />
+        })}
+      </div>
+    </article>
+  )
 }
